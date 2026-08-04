63627ca80de10375f7b79e392d5cf5e6eb37ae1c
shuvo794 <shuvohossain794@gmail.com>
2026-08-04 12:24:11 +0600

    feat: initialize project structure and configure Tailwind CSS with Nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,96 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const DATA = [
+  {
+    id: "1",
+    name: "John Doe",
+    roll: "101",
+    age: "20",
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    roll: "102",
+    age: "21",
+  },
+  {
+    id: "3",
+    name: "Alex Johnson",
+    roll: "103",
+    age: "19",
+  },
+  {
+    id: "4",
+    name: "Emily Davis",
+    roll: "104",
+    age: "22",
+  },
+];
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView>
+      <View>
+        <Text
+          style={{
+            fontSize: 20,
+            color: "red",
+            fontWeight: "bold",
+            textAlign: "center",
+            marginTop: 20,
+          }}
+        >
+          Assalamu Alaikum
+        </Text>
+      </View>
+      <TextInput
+        placeholder="Enter Your Name"
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginTop: 20,
+          marginLeft: 20,
+          marginRight: 20,
+        }}
+      />
+      <TouchableOpacity
+        style={{
+          height: 40,
+          backgroundColor: "#f4511e",
+          marginTop: 20,
+          marginLeft: 20,
+          marginRight: 20,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{ color: "white", textAlign: "center", marginTop: 10 }}>
+          Submit
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginTop: 20,
+              marginLeft: 20,
+              marginRight: 20,
+              borderRadius: 5,
+            }}
+          >
+            <Text style={{ color: "#f4511e" }}>Student Name: {item.name}</Text>
+            <Text style={{ color: "#666" }}>Roll No: {item.roll}</Text>
+            <Text style={{ color: "#666" }}>Age: {item.age}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 }
